26e9ddfa830a02a4894b24befe5eaeca9b45430f
Kartikdogra007 <117922918+Kartikdogra007@users.noreply.github.com>
2022-12-22 01:25:08 -0800

    Add files via upload

---
+++ frontend programs/jquery.php
@@ -0,0 +1,51 @@
+<head>
+    <script src="//code.jquery.com/jquery-1.11.1.min.js"></script>
+      <meta charset="utf-8">
+      <script>
+        $("p").bind("click", function(){
+   $( "<div>This is a click Event</div>").appendTo( "body" );
+});
+​
+$("p").bind("dblclick", function(){
+   $( "<div>This is a double-click Event</div>"  ).appendTo( "body" );
+});
+      </script>
+      <title>Scroll to the top of the page with jQuery</title>
+    </head>
+    <body>
+      <p>jquery</p>
+      <p>jquery</p>
+      <p>jquery</p>
+      <p>jquery</p>
+      <p>jquery</p>
+      <p>jquery</p>
+      <p>jquery</p>
+      <p>jquery</p>
+      <p>jquery</p>
+      <p>jquery</p>
+      <p>jquery</p>
+      <p>jquery</p>
+      <p>jquery</p>
+      <p>jquery</p>
+      <p>jquery</p>
+      <p>jquery</p>
+      <p>jquery</p>
+      <p>jquery</p>
+      <p>jquery</p>
+      <p>jquery</p>
+      <p>jquery</p>
+      <p>jquery</p>
+      <p>jquery</p>
+      <p>jquery</p>
+      <p>jquery</p>
+      <p>jquery</p>
+      <p>jquery</p>
+      <p>jquery</p>
+      <p>jquery</p>
+      <p>jquery</p>
+      <p>jquery</p>
+      <p>jquery</p>
+      <p>jquery</p>
+      <p>jquery</p>
+    <a href='#top'>Go Top</a>  
+    </body>
